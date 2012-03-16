cb8383d704fcc69b2227063e26cfc06d30dd53c0
Scott Robinson <sr@thoughtworks.com>
2012-03-16 21:41:38 +0800

    Remove: INVADER SWARM

--- world.lua
+++ world.lua
@@ -3,15 +3,11 @@ local __ = require 'underscore'
 local C = require 'consts'
 
 local Bullet = require 'bullet'
-local Swarm = require 'swarm'
 
 return function()
     local bullet = Bullet(500, 500, C.bullet.v)
 
-    local swarm = Swarm(C.swarm.initial.x,
-                        C.swarm.initial.y)
-
-    __.extend(Screen, {bullet, swarm})
+    __.extend(Screen, {bullet})
 
     await('key.escape')
 end
